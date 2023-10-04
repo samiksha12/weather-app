99ada73256b1f63709849e8a5c3125e8568bf18c
Samiksha Sapkota <sapkota.samiksha@gmail.com>
2023-10-03 19:28:04 -0500

    update

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 // export const BACKEND_DOMAIN = "http://localhost:3100";
 // export const BACKEND_DOMAIN = "http://192.168.1.164:3100";
-export const BACKEND_DOMAIN = "https://express-json-api.vercel.app/";
+export const BACKEND_DOMAIN = "https://express-json-api.vercel.app";
 
 export const loginCredentials = {
         userName:"Samiksha",
