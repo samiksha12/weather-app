69ade1283faafdd9523bb5f115fa44c03a883783
Samiksha Sapkota <sapkota.samiksha@gmail.com>
2023-10-03 19:58:08 -0500

    update

--- src/App.js
+++ src/App.js
@@ -74,9 +74,9 @@ function App() {
           const weatherCurrent = weatherData.data.filter(
             (weather) => weather.geonameId === city.geonameId
           );
-          if (weatherCurrent.length > 0) {
-            city.current_weather = weatherCurrent[0].current_weather;
-          }
+
+          city.current_weather = weatherCurrent[0].current_weather;
+
           return city;
         }
         return city;
@@ -95,7 +95,12 @@ function App() {
           typeof val !== undefined &&
             typeof val === "object" &&
             dispatch(
-              weatherApiAction(val.latitude, val.longitude,val.timezone, val.geonameId)
+              weatherApiAction(
+                val.latitude,
+                val.longitude,
+                val.timezone,
+                val.geonameId
+              )
             );
           dispatch(seeDetailApiAction("home"));
         }
