d92b4aa7e897fb3ee013b9b34711e04ddd32c6b8
Samiksha Sapkota <sapkota.samiksha@gmail.com>
2023-10-03 22:07:10 -0500

    backend url update

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 // export const BACKEND_DOMAIN = "http://localhost:3100";
 // export const BACKEND_DOMAIN = "http://192.168.1.164:3100";
-export const BACKEND_DOMAIN = "https://express-json-api-weather.vercel.app";
+export const BACKEND_DOMAIN = "https://fluffy-phase-delivery.glitch.me";
 
 export const loginCredentials = {
         userName:"Samiksha",
