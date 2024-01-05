47d4e47575462a047a25c27a55b738a9bee40c34
Samiksha Sapkota <sapkota.samiksha@gmail.com>
2024-01-04 20:59:29 -0600

    backend domain update

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
-export const BACKEND_DOMAIN = "http://localhost:3100";
+// export const BACKEND_DOMAIN = "http://localhost:3100";
 // export const BACKEND_DOMAIN = "http://192.168.1.164:3100";
-// export const BACKEND_DOMAIN = "https://fluffy-phase-delivery.glitch.me";
+export const BACKEND_DOMAIN = "https://fluffy-phase-delivery.glitch.me";
 
 export const loginCredentials = {
         userName:"Samiksha",
